0ac5b4143ff2af8d2630d8e0af04c26db74a396c
pengweihuang <pengweihuang@qudian.com>
2019-04-15 23:55:15 +0800

    替换注解为fastjson

--- xmutca-incubator-sso/src/main/java/com/xmutca/incubator/sso/dto/TokenResponseDto.java
+++ xmutca-incubator-sso/src/main/java/com/xmutca/incubator/sso/dto/TokenResponseDto.java
@@ -1,6 +1,6 @@
 package com.xmutca.incubator.sso.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.alibaba.fastjson.annotation.JSONField;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,30 +16,30 @@ public class TokenResponseDto {
     /**
      * 访问令牌
      */
-    @JsonProperty("access_token")
+    @JSONField(name = "access_token")
     private String accessToken;
 
     /**
      * 刷新令牌
      */
-    @JsonProperty("refresh_token")
+    @JSONField(name = "refresh_token")
     private String refreshToken;
 
     /**
      * 令牌类型
      */
-    @JsonProperty("token_type")
+    @JSONField(name = "token_type")
     private String tokenType;
 
     /**
      * 过期时间
      */
-    @JsonProperty("expires_in")
+    @JSONField(name = "expires_in")
     private long expiresIn;
 
     /**
      * 刷新过期时间
      */
-    @JsonProperty("refresh_expires_in")
+    @JSONField(name = "refresh_expires_in")
     private long refreshExpiresIn;
 }
