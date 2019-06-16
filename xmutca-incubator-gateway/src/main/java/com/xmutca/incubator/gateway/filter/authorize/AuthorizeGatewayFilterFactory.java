2779681e6f2012b77186b2790e9330182bf64106
pengweihuang <pengweihuang@qudian.com>
2019-06-16 11:00:17 +0800

    精简网关配置

--- xmutca-incubator-gateway/src/main/java/com/xmutca/incubator/gateway/filter/authorize/AuthorizeGatewayFilterFactory.java
+++ xmutca-incubator-gateway/src/main/java/com/xmutca/incubator/gateway/filter/authorize/AuthorizeGatewayFilterFactory.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFac
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
 
 import java.util.List;
 
@@ -23,8 +22,6 @@ import java.util.List;
 @Component
 public class AuthorizeGatewayFilterFactory extends AbstractGatewayFilterFactory<AuthorizeGatewayFilterFactory.Config> implements Ordered {
 
-    public static final AntPathMatcher matcher = new AntPathMatcher();
-
     public AuthorizeGatewayFilterFactory() {
         super(Config.class);
     }
@@ -32,7 +29,7 @@ public class AuthorizeGatewayFilterFactory extends AbstractGatewayFilterFactory<
     @Override
     public GatewayFilter apply(Config config) {
         // 没值，直接认为不做权限处理
-        if (StringUtils.isBlank(config.getAuthorizeCode()) || config.getUri().isEmpty()) {
+        if (StringUtils.isBlank(config.getAuthorizeCode())) {
             return (exchange, chain) -> chain.filter(exchange);
         }
 
@@ -41,14 +38,6 @@ public class AuthorizeGatewayFilterFactory extends AbstractGatewayFilterFactory<
             String user = exchange.getRequest().getHeaders().getFirst(RequestConstant.REQUEST_HEADER_USER);
             if (StringUtils.isBlank(user)) {
                 return ResultUtils.build401Result(exchange);
-            }
-
-            // 获取路径判断
-            String requestUri = exchange.getRequest().getPath().value();
-            for (String pattern : config.getUri()) {
-                if (!matcher.match(pattern, requestUri)) {
-                    return ResultUtils.build403Result(exchange);
-                }
             }
 
             // 获取用户的权限判断
@@ -73,11 +62,6 @@ public class AuthorizeGatewayFilterFactory extends AbstractGatewayFilterFactory<
     public static class Config {
 
         public static final String DEFAULT_AUTHORIZE_KEY = "X-Request-Authorize";
-
-        /**
-         * 请求地址
-         */
-        private List<String> uri = Lists.newArrayList();
 
         /**
          * 权限码
